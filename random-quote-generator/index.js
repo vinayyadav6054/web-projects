d97720669a0bba9e634d426857191a450206924e
vinayyadav6054 <yvinay7271@gmail.com>
2025-01-04 21:30:09 +0530

    random quotes generator project completed

---
+++ random-quote-generator/index.js
@@ -0,0 +1,34 @@
+const btnEl = document.getElementById("btn");
+const quoteEl = document.getElementById('quote');
+const authorEl = document.getElementById("author");
+
+const apiUrl = "http://api.quotable.io/random";
+
+async function getQuote(){
+    // console.log("clicked")
+    try {
+        btnEl.innerText ="Loading...";
+        btnEl.disabled = true;
+        quoteEl.innerText = "Updating...";
+        authorEl.innerText = "Updating...";
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const quoteContent = data.content;
+        const quoteAuthor = data.author;
+        quoteEl.innerText = quoteContent;
+        authorEl.innerText = "~" + quoteAuthor;
+        btnEl.innerText ="Get a quote";
+        btnEl.disabled = false;
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+        quoteEl.innerText = "An error happened, try again later";
+        authorEl.innerText = "An error happened";
+        btnEl.innerText ="Get a quote";
+        btnEl.disabled = false;
+    }   
+}
+
+getQuote();
+
+btnEl.addEventListener("click", getQuote);
